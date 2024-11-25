23647e258d5b15fec5bc363d1cfbacfdb9164901
BritishWerewolf <britishwerewolfdev@gmail.com>
2024-11-25 22:41:33 +0000

    Add option to maintain aspect ratio on resize (#971)
    * Add option to resize one dimension and maintain aspect ratio.
    
    * Update src/utils/image.js
    
    Reduce function calls for optimisation.
    
    Co-authored-by: Joshua Lochner <admin@xenova.com>
    
    * Optimise image utils tests.
    
    * nullish dimension may be `null`, `undefined`, or `-1`
    
    ---------
    
    Co-authored-by: Joshua Lochner <admin@xenova.com>

--- src/utils/core.js
+++ src/utils/core.js
@@ -1,10 +1,10 @@
 
 /**
  * @file Core utility functions/classes for Transformers.js.
- * 
+ *
  * These are only used internally, meaning an end-user shouldn't
  * need to access anything here.
- * 
+ *
  * @module utils/core
  */
 
@@ -65,7 +65,7 @@ export function escapeRegExp(string) {
  * Check if a value is a typed array.
  * @param {*} val The value to check.
  * @returns {boolean} True if the value is a `TypedArray`, false otherwise.
- * 
+ *
  * Adapted from https://stackoverflow.com/a/71091338/13989043
  */
 export function isTypedArray(val) {
@@ -80,6 +80,15 @@ export function isTypedArray(val) {
  */
 export function isIntegralNumber(x) {
     return Number.isInteger(x) || typeof x === 'bigint'
+}
+
+/**
+ * Determine if a provided width or height is nullish.
+ * @param {*} x The value to check.
+ * @returns {boolean} True if the value is `null`, `undefined` or `-1`, false otherwise.
+ */
+export function isNullishDimension(x) {
+    return x === null || x === undefined || x === -1;
 }
 
 /**
@@ -151,9 +160,9 @@ export function calculateReflectOffset(i, w) {
 }
 
 /**
- * 
- * @param {Object} o 
- * @param {string[]} props 
+ *
+ * @param {Object} o
+ * @param {string[]} props
  * @returns {Object}
  */
 export function pick(o, props) {
@@ -170,7 +179,7 @@ export function pick(o, props) {
 /**
  * Calculate the length of a string, taking multi-byte characters into account.
  * This mimics the behavior of Python's `len` function.
- * @param {string} s The string to calculate the length of. 
+ * @param {string} s The string to calculate the length of.
  * @returns {number} The length of the string.
  */
 export function len(s) {
